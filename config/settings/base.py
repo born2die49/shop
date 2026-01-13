b869912d5738e6802d8c9a2c79d3397ed6eb353a
jamisays <jamisays@gmail.com>
2026-01-13 13:59:42 +0600

    feature: add apps and logging

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,7 +44,11 @@ THIRD_PARTY_APPS = [
   "storages"
 ]
 
-LOCAL_APPS = []
+LOCAL_APPS = [
+    "core_apps.user",
+    "core_apps.common",
+    "core_apps.profiles",
+]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
@@ -83,8 +87,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': getenv("POSTGRES_DB"),
+        'USER': getenv("POSTGRES_USER"),
+        'PASSWORD': getenv("POSTGRES_PASSWORD"),
+        'HOST': getenv("POSTGRES_HOST"),
+        'PORT': getenv("POSTGRES_PORT"),
     }
 }
 
