5325ff2e0089f3cfa0814654b0e84c2fa437dbd5
jamisays <jamisays@gmail.com>
2026-01-16 09:03:50 +0600

    feature: add google OAuth login

--- core_apps/user/urls.py
+++ core_apps/user/urls.py
@@ -8,14 +8,14 @@ from .views import (
 )
 
 urlpatterns = [
-    path(
+    re_path(
         r"^o/(?P<provider>\S+)/$",
         CustomProviderAuthView.as_view(),
         name="provider-auth",
     ),
     path("customer-login/", CustomerLoginView.as_view(), name="customer_login"),
     path("staff-login/", StaffLoginView.as_view(), name="staff_login"),
-    path("social/", CustomProviderAuthView.as_view(), name="social_login"),
+    # path("social/", CustomProviderAuthView.as_view(), name="social_login"),
     path("refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
     path("logout/", LogoutAPIView.as_view(), name="logout"),
 ]
